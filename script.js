09ab2a3f38228a3fa042327285046b8a67734465
Jonathan Taylor <jon.1807@hotmail.co.uk>
2024-08-16 15:25:44 +0100

    1st upload of the week 1 project

---
+++ script.js
@@ -0,0 +1,18 @@
+// this was copied from w3schools https://www.w3schools.com/howto/howto_js_scroll_to_top.asp just to see if i could get it to work
+let mybutton = document.getElementById("top");
+window.onscroll = function () {
+  scrollfunction();
+};
+function scrollfunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topfunction() {
+  document.documentElement.scrollTop = 0;
+}
